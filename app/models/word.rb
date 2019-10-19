5f97e08868ccf5900495a1f8ff8e6f2176f4c689
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-10-19 11:29:19 -0400

    Was able to finalize the game and get backend to push sample word to frontend

--- app/models/word.rb
+++ app/models/word.rb
@@ -10,14 +10,14 @@ class Word < ApplicationRecord
 
 # RAN THIS CODE ONCE - PURPOSE: TO GET RESPONSE STRING, SPLIT LINE 
 # AND CREATE EACH WORD IN MY DATABASE
-    # def self.all_words 
-    #     resp = RestClient.get("http://app.linkedin-reach.io/words", headers={})
-    #     words_array= resp.body.split("\n")
+    def self.all_words 
+        resp = RestClient.get("http://app.linkedin-reach.io/words")
+        words_array= resp.body.split("\n")
 
 
-    #     # .EACH OVER THEM AND .CREATE THEM
-    #     words_array.each{ | word | Word.create({'text': word})}
+        # .EACH OVER THEM AND .CREATE THEM
+        words_array.each{ | word | Word.create({'text': word})}
         
-    # end 
+    end 
 
 end
