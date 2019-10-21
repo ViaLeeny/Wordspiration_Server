4c5229cc6464bfca4a6e480ce5b216408101de9f
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-10-20 22:49:36 -0400

    Added update_gameS_score capabilities

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class GamesController < ApplicationController
 
     def index 
@@ -17,7 +18,7 @@ class GamesController < ApplicationController
     def create 
         player = Player.find_or_create_by(name: params[:playerName])
         theWord = Word.find_or_create_by(text: params[:word])
-        game = Game.new(player_id: player.id, word_id: theWord.id)
+        game = Game.new(player_id: player.id, word_id: theWord.id, score: 0)
 
         if game.valid? 
             game.save 
@@ -25,5 +26,20 @@ class GamesController < ApplicationController
         else
             render json: {error: "Game invalid."}
         end 
+    end 
+
+
+    def update 
+
+        puts 'hello controller edit'
+        game = Game.find_by(id: params[:id])
+        
+        # if game
+            game.update(player_id: params[:player_id], word_id: params[:word_id], score: params[:score]) 
+            render json: game, include: [:player, :word]
+        # else 
+        #     render json: { error: "Game doesn't exist" }
+        # end 
+        binding.pry
     end 
 end
